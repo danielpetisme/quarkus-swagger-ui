380b935fffe504714534d13a60fe61ce7cf21ead
Daniel Petisme <daniel.petisme@gmail.com>
2019-03-15 22:16:16 +0100

    Initial commit
    Signed-off-by: Daniel Petisme <daniel.petisme@gmail.com>

---
+++ deployment/src/main/java/io/quarkus/swaggerui/deployment/SwaggerUiProcessor.java
@@ -0,0 +1,10 @@
+package io.quarkus.swaggerui.deployment;
+
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+public class SwaggerUiProcessor {
+
+    @ConfigRoot(name = "swaggerui")
+    static final class SwaggerUiConfig {
+    }
+}
